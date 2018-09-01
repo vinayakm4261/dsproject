fec82eacc583c1ed935955efc9a5d85ee2c14a87
vinayakm4261 <42168249+vinayakm4261@users.noreply.github.com>
2018-09-01 11:11:40 +0530

    Use this file for writing greeting page ka code

---
+++ greetingpage.c
@@ -0,0 +1,16 @@
+//Main greeting page for program
+//Include header files
+void main()
+{
+    int i,j,k;//Declaring just in case
+    printf("\nWelcome to the.....");
+    //Bhai ye bana
+    /*.
+    .
+    .
+    .Write something in here lol..!!
+    .
+    .
+    .*/
+    menu();
+}
